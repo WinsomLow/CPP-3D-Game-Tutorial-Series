909930ee6b421947822c0aba57fcad2d75fb5a83
PardCode <45696173+PardCode@users.noreply.github.com>
2020-08-01 19:24:23 +0200

    Tutorial 22 Special Demo MultiLayered Animated Earth
    Tutorial 22 Special Demo MultiLayered Animated Earth

---
+++ Tutorial22_SpecialDemo_MultiLayered_Animated_Earth/AppWindow.h
@@ -0,0 +1,118 @@
+/*MIT License
+
+C++ 3D Game Tutorial Series (https://github.com/PardCode/CPP-3D-Game-Tutorial-Series)
+
+Copyright (c) 2019-2020, PardCode
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.*/
+
+
+#pragma once
+#include "Window.h"
+#include "GraphicsEngine.h"
+#include "SwapChain.h"
+#include "DeviceContext.h"
+#include "VertexBuffer.h"
+#include "IndexBuffer.h"
+#include "ConstantBuffer.h"
+#include "VertexShader.h"
+#include "PixelShader.h"
+#include "InputListener.h"
+#include "Matrix4x4.h"
+
+
+class AppWindow: public Window,public InputListener
+{
+public:
+	AppWindow();
+	~AppWindow();
+
+	// Inherited via Window
+	virtual void onCreate() override;
+	virtual void onUpdate() override;
+	virtual void onDestroy() override;	
+	virtual void onFocus() override;
+	virtual void onKillFocus() override;
+	virtual void onSize() override;
+
+
+	// Inherited via InputListener
+	virtual void onKeyDown(int key) override;
+	virtual void onKeyUp(int key) override;
+	virtual void onMouseMove(const Point& mouse_pos) override;
+
+	virtual void onLeftMouseDown(const Point& mouse_pos) override;
+	virtual void onLeftMouseUp(const Point& mouse_pos) override;
+	virtual void onRightMouseDown(const Point& mouse_pos) override;
+	virtual void onRightMouseUp(const Point& mouse_pos) override;
+public:
+	void render();
+	void update();
+	void updateModel();
+	void updateCamera();
+	void updateSkyBox();
+
+	void drawMesh(const MeshPtr& mesh, const VertexShaderPtr& vs, const PixelShaderPtr & ps, const ConstantBufferPtr& cb,
+		 const TexturePtr* list_tex, unsigned int num_textures);
+
+private:
+	SwapChainPtr m_swap_chain;
+	VertexShaderPtr m_vs;
+	PixelShaderPtr m_ps;
+	PixelShaderPtr m_sky_ps;
+	ConstantBufferPtr m_cb;
+	ConstantBufferPtr m_sky_cb;
+	TexturePtr m_earth_color_tex;
+	TexturePtr m_earth_spec_tex;
+	TexturePtr m_earth_night_tex;
+	TexturePtr m_clouds_tex;
+
+	TexturePtr m_sky_tex;
+	MeshPtr m_mesh;
+	MeshPtr m_sky_mesh;
+private:
+	long m_old_delta;
+	long m_new_delta;
+	float m_delta_time;
+
+	float m_delta_pos;
+	float m_delta_scale;
+	float m_delta_rot;
+
+	float m_rot_x=0.0f;
+	float m_rot_y = 0.0f;
+
+	float m_light_rot_y = 0.0f;
+
+
+	float m_scale_cube = 1;
+	float m_forward = 0.0f;
+	float m_rightward = 0.0f;
+
+	bool cull = true;
+	Matrix4x4 m_world_cam;
+	Matrix4x4 m_view_cam;
+	Matrix4x4 m_proj_cam;
+
+	float m_time = 0.0f;
+
+	bool m_play_state = false;
+	bool m_fullscreen_state = false;
+};
+
